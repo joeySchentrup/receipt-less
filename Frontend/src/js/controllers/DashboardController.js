6cf2193f4013aeb77f3ededd0843bf8183a29eb4
NaturalFractals <jcochran800@gmail.com>
2018-01-20 16:10:58 -0500

    Updated DashBoardController, main.html and profile page.

--- Frontend/src/js/controllers/DashboardController.js
+++ Frontend/src/js/controllers/DashboardController.js
@@ -7,6 +7,8 @@ DashboardController.$inject = ['$scope', '$http'];
 function DashboardController($scope, $http) {
   $scope.name = "Joseph";
   $scope.number = 4;
+  $scope.names = ['Sort Ascending', 'Sort Decending', 'Sort Date', 'Sort Price']
+  $scope.selectedName = 'Sort Date';
 
   $scope.getNumber = function(num) {
       return new Array(num);   
