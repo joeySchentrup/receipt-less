cde1704624517f30378bb867838c90dcc56cc635
joeySchentrup <j03y@live.com>
2018-01-21 10:07:20 -0500

    Fixec start.bash

--- start.bash
+++ start.bash
@@ -4,11 +4,11 @@ git pull
 
 cd Backend
 docker run --name mongo -d mongo
-docker run --link mongo:mongo -p 8081:8081 mongo-express
+docker run --link mongo:mongo -p 8081:8081 -d mongo-express
 
 docker build -t receipt-less-backend:1 .
-docker run -p 8000:8000 --name backend --link mongo:mongo receipt-less:1
+docker run -p 8000:8000 -d --name backend --link mongo:mongo receipt-less:1
 
 cd ../Frontend
-docker build -t gulp:1 -f ./gulp.dockerfile .
-docker run -p 80:80 gulp:1
+docker build -t receipt-less-frontend:1 .
+docker run -p 80:80 -d --name frontend receipt-less-frontend:1
