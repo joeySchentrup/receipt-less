5965b84cf22bcf5b97d956a58967ae8c4cbb4f76
NaturalFractals <jcochran800@gmail.com>
2018-01-21 03:45:03 -0500

    Working Dashboard. Bruv.

--- Frontend/src/js/controllers/DashboardController.js
+++ Frontend/src/js/controllers/DashboardController.js
@@ -7,6 +7,7 @@ DashboardController.$inject = ['$scope', '$http', 'Notification'];
 function DashboardController($scope, $http, Notification) {
   $scope.name = "Joseph";
   $scope.number = 4;
+  $scope.searchText = "";
 
   $scope.receipts = [];
   $scope.currentReceipts = [];
@@ -67,12 +68,21 @@ function DashboardController($scope, $http, Notification) {
     }
   }
 
-  $scope.test = function() {
-    var url = 'http://165.227.206.185:8000/account/jos1@ufl.edu';
+  var test = function() {
+    var url = 'http://165.227.206.185:8000/receipts/jose13651';
     $http.get(url)
       .then(response => {
-        $scope.receipts = response.data;
-        $scope.currentReceipts = response.data;
+        console.log(response);
+        var current = {};
+        for(var i = 0; i < response.data.length; i++) {
+            current = {};
+            current.businessName = response.data[i].business_name; 
+            current.itemName = response.data[i].item_name;
+            current.amount = response.data[i].amount;
+            current.date = response.data[i].date_and_time;
+            $scope.currentReceipts.push(current);
+        }
+        this.receipts = this.currentReceipts;
         Notification.success({message: 'Success!'});
       })
       .catch(function(err) {
@@ -81,14 +91,16 @@ function DashboardController($scope, $http, Notification) {
       });
   }
 
+  test();
+
   $scope.search = function(str) {
     var searchArray = [];
     $scope.receipts.forEach(rec => {
-      if(rec.businessName.contains(str) || rec.itemName.contains(str)) {
-        searchArray.push[];
+      console.log(rec.businessName);
+      if(rec.businessName.includes(str) || rec.itemName.includes(str)) {
+        searchArray.push(rec);  
       }
-      $scope.currentReceipts = searchArray;
     });
-
+    $scope.currentReceipts = searchArray;
   }
 }
