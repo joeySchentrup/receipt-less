85c87b02ea42663661e7f528e85e105fe8263a53
jmcacioppo <jmcacioppo0531@gmail.com>
2018-01-21 02:15:22 -0500

    Fixed UI for add receipts and expenses

--- Frontend/src/js/controllers/ExpensesController.js
+++ Frontend/src/js/controllers/ExpensesController.js
@@ -7,17 +7,22 @@ ExpensesController.$inject = ['$scope', '$http'];
 function ExpensesController($scope, $http) {
   $scope.name = "Joseph";
 
-  $scope.labels = ["Groceries", "Movies", "School"];
-  $scope.data = [300, 500, 100];
+  $scope.pieLabels = ["Groceries", "Movies", "School"];
+  $scope.pieData = [300, 500, 100];
+
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-  $scope.series = ['Series A', 'Series B'];
+  $scope.series = ['Groceries', 'Movies', 'School'];
+
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
+    [28, 48, 40, 19, 86, 27, 90],
+    [14, 15, 52, 52, 64, 21, 11]
   ];
+
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
+
   $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
   $scope.options = {
     scales: {
