2799c0f40b176181253b953b1b9dd36a084762dc
joeySchentrup <j03y@live.com>
2018-02-02 09:38:33 -0500

    Got rid of src folder. Works locally now.

--- Frontend/server.js
+++ Frontend/server.js
@@ -3,6 +3,6 @@ var path = require('path');
 var app = express();
 var rootPath = path.normalize(__dirname);
 
-app.use(express.static(rootPath + '/src'));
+app.use(express.static(rootPath));
 app.listen(80);
 console.log("Listening at localhost:80..."); 
